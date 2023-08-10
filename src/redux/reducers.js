e2cf30a83474e64431839bfb2b8722493dbe455c
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-08-10 12:20:08 +0100

    project done .

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,25 +2,10 @@ import data from '@/data/data.json';
 
 export const SearchAirbnb = (state, { payload }) => {
   let arr = [];
-  let arr2 = [];
-  let { location, guest } = payload;
+  let { location } = payload;
   for (let i = 0; i < data.length; i++) {
-    if (location === '' || guest === '') {
+    if (location === '') {
       state.locationFound = [];
-      state.guestFound = [];
-    } else if (
-      (guest !== '' &&
-        data[i].city.toLowerCase().includes(location.toLowerCase())) ||
-      data[i].country.toLowerCase().includes(location.toLowerCase())
-    ) {
-      arr.push(data[i].city + ' ' + data[i].country);
-      for (let i = 0; i < data.length; i++) {
-        if (parseInt(guest) < data[i].maxGuests) {
-          arr.push(data[i].city + ' ' + data[i].country);
-        }
-      }
-
-      arr.concat(arr2);
     } else if (
       data[i].city.toLowerCase().includes(location.toLowerCase()) ||
       data[i].country.toLowerCase().includes(location.toLowerCase())
